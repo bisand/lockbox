8d61eb6745f5094d498f47861766fc59249097b3
André Biseth <andre@biseth.net>
2022-02-18 07:39:57 +0100

    first commit

---
+++ Lockbox.Shell/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lockbox.Test;
+
+
+var test = new PdfExtractor();
+test.Execute();
---
+++ Lockbox.Test/Class1.cs
@@ -0,0 +1,38 @@
+﻿using PuppeteerSharp;
+
+namespace Lockbox.Test;
+
+public class PdfExtractor
+{
+    public bool async Execute()
+    {
+        Console.WriteLine("Hello, World!");
+        var options = new LaunchOptions
+        {
+            Headless = true
+        };
+
+        Console.WriteLine("Downloading chromium");
+        using var browserFetcher = new BrowserFetcher();
+        await browserFetcher.DownloadAsync();
+
+        Console.WriteLine("Navigating google");
+        using (var browser = await Puppeteer.LaunchAsync(options))
+        using (var page = await browser.NewPageAsync())
+        {
+            await page.GoToAsync("http://www.google.com");
+
+            Console.WriteLine("Generating PDF");
+            await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "google.pdf"));
+
+            Console.WriteLine("Export completed");
+
+            if (!args.Any(arg => arg == "auto-exit"))
+            {
+                Console.ReadLine();
+            }
+        }
+
+        return true;
+    }
+}
